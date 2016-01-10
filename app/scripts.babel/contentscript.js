ffc63065d064618c57240443362b4ecce12963d3
Scott Feinberg <scott.feinberg@nytimes.com>
2016-01-10 11:19:01 -0500

    Added the ability to set the userId of the chrome extension

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var metas = document.getElementsByTagName('meta'); 
+var metas = document.getElementsByTagName('meta');
 
 var i;
 
@@ -24,31 +24,34 @@ var timeInMs = Date.now();
 function closeIt()
 {
 	if (title && url && body ){
-		var newTimeInMs = Date.now();
-		var timeElapsed = Math.round((newTimeInMs - timeInMs)/1000);
-		var scrollEnd = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
-		var scrolledPer = Math.round(scrollEnd/scrollHeight*100);
+		chrome.storage.sync.get('userId', function (obj) {
+		    var userId = obj.userId;
+			var newTimeInMs = Date.now();
+			var timeElapsed = Math.round((newTimeInMs - timeInMs)/1000);
+			var scrollEnd = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+			var scrolledPer = Math.round(scrollEnd/scrollHeight*100);
 
-		var mypostrequest = new XMLHttpRequest();
-		mypostrequest.onreadystatechange=function(){
-		 if (mypostrequest.readyState===4){
-		  if (mypostrequest.status===200 || window.location.href.indexOf('http')===-1){
-		   
-		  }
-		 }
-		};
-		mypostrequest.open('POST', 'https://api.airtable.com/v0/appmOYXlT9Xpr8VLG/Sessions?api_key=keyNb38YSpAFdx34A', true);
-		mypostrequest.setRequestHeader('Content-type', 'application/json');
-		mypostrequest.send(JSON.stringify({'fields': {
-		    'URL': url,
-		    'Progress Percentage': scrolledPer,
-		    'Time Spent (Seconds)': timeElapsed,
-		    'User ID': [
-		      'recqRdm5XO0N8s3Hy'
-		    ],
-		    'Page Name': title
-		  }
-		}));
+			var mypostrequest = new XMLHttpRequest();
+			mypostrequest.onreadystatechange=function(){
+			 if (mypostrequest.readyState===4){
+			  if (mypostrequest.status===200 || window.location.href.indexOf('http')===-1){
+			   
+			  }
+			 }
+			};
+			mypostrequest.open('POST', 'https://api.airtable.com/v0/appmOYXlT9Xpr8VLG/Sessions?api_key=keyNb38YSpAFdx34A', true);
+			mypostrequest.setRequestHeader('Content-type', 'application/json');
+			mypostrequest.send(JSON.stringify({'fields': {
+			    'URL': url,
+			    'Progress Percentage': scrolledPer,
+			    'Time Spent (Seconds)': timeElapsed,
+			    'User ID': [
+			      String(userId)
+			    ],
+			    'Page Name': title
+			  }
+			}));
+		});
 	}
 	return;
 }
--- app/scripts.babel/options.js
+++ app/scripts.babel/options.js
@@ -1,3 +1,37 @@
 'use strict';
+// Saves options to chrome.storage
+function save_options() {
+  var color = document.getElementById('color').value;
+  var likesColor = document.getElementById('like').checked;
+  var userId = document.getElementById('userId').value;
+  chrome.storage.sync.set({
+    favoriteColor: color,
+    likesColor: likesColor,
+    userId: userId,
+  }, function() {
+    // Update status to let user know options were saved.
+    var status = document.getElementById('status');
+    status.textContent = 'Options saved.';
+    setTimeout(function() {
+      status.textContent = '';
+    }, 750);
+  });
+}
 
-console.log('\'Allo \'Allo! Option');
+// Restores select box and checkbox state using the preferences
+// stored in chrome.storage.
+function restore_options() {
+  // Use default value color = 'red' and likesColor = true.
+  chrome.storage.sync.get({
+    favoriteColor: 'red',
+    likesColor: true,
+    userId: 'recqRdm5XO0N8s3Hy',
+  }, function(items) {
+    document.getElementById('color').value = items.favoriteColor;
+    document.getElementById('like').checked = items.likesColor;
+    document.getElementById('userId').value = items.userId;
+  });
+}
+document.addEventListener('DOMContentLoaded', restore_options);
+document.getElementById('save').addEventListener('click',
+    save_options);
